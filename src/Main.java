6ef916930d3bf85bf38c4a0d8be8e5bd0079bdde
Simon Greene <simongreene02@gmail.com>
2015-09-19 11:04:24 -0500

    Factorial Function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Number:");
+		int input = scanner.nextInt();
+		if (input < 0) {
+			throw new IllegalArgumentException(input + "");
+		}
+		int output = 1;
+		for (int i = input; i > 0; i--) {
+			output = output * i;
+		}
+		System.out.println(input + "! = " + output);
 	}
 
 }
